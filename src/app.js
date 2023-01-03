5ca09c731204c0a6e87ae71e36c8c3a0fa8e4885
noedavico <noedavico@hotmail.com>
2023-01-03 20:35:06 +0000

    mensaje

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let palo = ["♦", "♥", "♠", "♣"];
+  let number = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  //Declaracion de arrays
+  let rand_palo = Math.floor(Math.random() * palo.length);
+  let rand_number = Math.floor(Math.random() * number.length);
+  //radom
+  let upper = document.querySelector(".top");
+  let numbers = document.querySelector(".number");
+  let lower = document.querySelector(".bottom");
+  //obtencion de elementos
+  numbers.innerHTML = number[rand_number];
+  upper.innerHTML = palo[rand_palo];
+  lower.innerHTML = palo[rand_palo];
+  //manipulacion de elementos
 };
